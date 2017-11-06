b47d2b5a87f7a01402ad936282f0032d43d6a4aa
M.Kumar <m.kumar@in.imshealth.com>
2017-11-06 14:43:48 +0530

    taking a random dataset and key element. code changes to fix bounary value cases

--- BinarySearchNLinearSearch/BinarySearch.py
+++ BinarySearchNLinearSearch/BinarySearch.py
@@ -1,23 +1,27 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+import random
+
+n = 1000
+a = random.sample(range(1, 100000), n)
 
 
-def get_mid(arr, low_ele, high_element):
-    return arr[round((arr.index(low_ele) + arr.index(high_element)) / 2+0.5)]
+def get_mid(arr, low_ind, high_ind):
+    return arr[round((low_ind + high_ind) / 2)]
 
 
-key = 1
+key = random.randint(1, 100000)
+print("key element is " + str(key))
 low = a[0]
-high = a[len(a) - 1]
-mid = get_mid(a, low, high)
+high = a[n - 1]
+mid = get_mid(a, 0, n-1)
 
 while low < high:
     print("mid_element is : " + str(mid))
     if mid > key:
-        high = mid
-        mid = get_mid(a, low, high)
+        high = a[a.index(mid)-1]
+        mid = get_mid(a, a.index(low), a.index(high))
     elif mid < key:
-        low = mid
-        mid = get_mid(a, low, high)
+        low = a[a.index(mid)+1]
+        mid = get_mid(a, a.index(low), a.index(high))
     else:
         break
 
