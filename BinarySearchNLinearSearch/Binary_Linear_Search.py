8f2983508f312ddcdad30405bdbab4e047db5096
M.Kumar <m.kumar@in.imshealth.com>
2017-11-12 16:29:08 +0530

    added code to run for 10 different input size.

--- BinarySearchNLinearSearch/Binary_Linear_Search.py
+++ BinarySearchNLinearSearch/Binary_Linear_Search.py
@@ -10,9 +10,11 @@ def binary_search(arr, length, key):
     low = arr[0]
     high = arr[length - 1]
     mid = get_mid(arr, 0, length - 1)
+    i = 0
 
     while low < high:
-        print("mid_element is : " + str(mid))
+        i += 1
+        # print("mid_element is : " + str(mid))
         if mid > key:
             high = arr[arr.index(mid) - 1]
             mid = get_mid(arr, arr.index(low), arr.index(high))
@@ -23,21 +25,28 @@ def binary_search(arr, length, key):
             break
 
     print("binary search: element found at : " + str(arr.index(mid) + 1))
+    return i
 
 
 def linear_search(arr, length, key):
+    i = 0
     for i in range(length):
         if arr[i] == key:
             print("linear search : element found at : " + str(i + 1))
             break
+    return i + 1
 
 
 def main():
-    n = 1000
-    a = sorted(random.sample(range(1, 100000), n))
-    key = random.choice(a)
-    binary_search(a, n, key)
-    linear_search(a, n, key)
+    for n in range(1000, 2100, 100):
+        a = sorted(random.sample(range(1, 100000), n))
+        print("size of the array is %d" % len(a))
+        key = random.choice(a)
+        b_search = binary_search(a, n, key)
+        l_search = linear_search(a, n, key)
+        print("b_search search took %d iteration while linear search took %d iterations for input size %d \n"
+              % (b_search, l_search, n))
+
 
 if __name__ == "__main__":
     main()
