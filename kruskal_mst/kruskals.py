fa96988b508e839b510c588ab2d70f48003abcb9
M.Kumar <m.kumar@in.imshealth.com>
2018-01-07 10:25:18 +0530

    Kruskals algorithm completed.

--- kruskal_mst/kruskals.py
+++ kruskal_mst/kruskals.py
@@ -1,4 +1,4 @@
-class graph:
+class Graph:
     def __init__(self, vertex):
         self.V = vertex
         self.graph = []
@@ -8,14 +8,28 @@ class graph:
             return i
         return self.find(parent, parent[i])
 
-    def union(self, parent, u, v):
+    @staticmethod
+    def union_op(parent, rank, u, v):
+        if rank[u] > rank[v]:
+            parent[v] = u
+        if rank[u] < rank[v]:
+            parent[u] = v
+        else:
+            parent[u] = v
+            rank[v] += 1
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
 
     def kruskal_mst(self):
         self.graph = sorted(self.graph, key=lambda items: items[2])
         parent = []
+        rank = []
+        result = []
 
         for node in range(self.V):
-            parent = parent[node]
+            parent.append(node)
+            rank.append(0)
 
         e = 0
         i = 0
@@ -26,5 +40,17 @@ class graph:
             y = self.find(parent, edge[1])
 
             if x != y:
-                result = self.graph[i]
-                self.union(parent, x, y)
+                e += 1
+                result.append(edge)
+                self.union_op(parent, rank, x, y)
+        return result
+
+
+g = Graph(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+
+print(g.kruskal_mst())
